35f2a8a5d76d03acd649d106d6b2e241f883d42e
niki <145340809+huntergreyn@users.noreply.github.com>
2024-08-04 15:49:14 +0330

    upload calculator project

---
+++ calc-project/script.js
@@ -0,0 +1,11 @@
+let calc = 0, num1 = prompt("Enter First Number:") , num2 = prompt("Enter Second Number:")
+
+console.log(" + :", num1 + num2);
+
+console.log(" - :", num1 - num2);
+
+console.log(" * :", num1  * num2);
+
+console.log(" / :", num1 / num2);
+
+console.log(" % :", num1 % num2);
